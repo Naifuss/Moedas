cce6257d7bc31a0a9141f06efbe448987f16f84c
Naifuss <124268998+Naifuss@users.noreply.github.com>
2023-11-22 14:09:51 +0000

    aaaaa

--- App.js
+++ App.js
@@ -37,27 +37,27 @@ export default function App() {
   }
 
   return (
-    <View style={{ flex: 1, backgroundColor: "#000000", justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, backgroundColor: "#1A6AF6", justifyContent: 'center', alignItems: 'center' }}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontWeight: "bold", textAlign: 'center', alignSelf: 'center', fontSize: 35, color: '#FFFFFF' }}>Moedas</Text>
+        <Text style={{ fontWeight: "bold", textAlign: 'center', alignSelf: 'center', fontSize : 35, backgroundColor: '#fff ' , paddingBottom:40,}}>Conversor de Moedas</Text>
         <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-          <Text style={{ color: '#FFFFFF', fontSize: 20, }}>Moeda 1:</Text>
-          <View style={{ borderColor: '#FFFFFF', borderStyle: "solid", borderWidth: 2, borderRadius: 9 }}>
+          <Text style={{ color: '#FFFFFF', fontSize: 30, }}>Moeda 1:</Text>
+          <View style={{ borderColor: '#FFFFFF', borderStyle:'solid', borderWidth: 1, borderRadius: 9 }}>
             <Picker
               selectedValue={moedaOrigem}
               onValueChange={(itemValue, itemIndex) => setMoedaOrigem(itemValue)}
               style={{ width: 200, color: "#FFFFFF" }}
             >
               <Picker.Item label="Real Brasileiro" value="BRL" />
-              <Picker.Item label="Dolar Americano" value="USD" />
+             <Picker.Item label="Dolar Americano" value="USD" />
               <Picker.Item label="Ouro" value="XAU" />
               <Picker.Item label="Bitcoin" value="BTC" />
             </Picker>
           </View>
         </View>
         <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-          <Text style={{ color: '#FFFFFF', fontSize: 20, }}>Moeda 2:</Text>
-          <View style={{ borderColor: '#FFFFFF', borderStyle: "solid", borderWidth: 2, borderRadius: 9 }}>
+          <Text style={{ color: '#FFFFFF', fontSize: 30, }}>Moeda 2:</Text>
+          <View style={{ borderColor: '#FFFFFF', borderStyle: "solid", borderWidth: 1, borderRadius: 9 ,}}>
             <Picker
               selectedValue={moedaDestino}
               onValueChange={(itemValue) => setMoedaDestino(itemValue)}
@@ -77,10 +77,10 @@ export default function App() {
           </View>
         </View>
         <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
-          <Pressable style={{ backgroundColor: '#32CD32', width: 125, borderRadius: 20, marginTop: 20, height: 30, justifyContent: 'center', alignItems: 'center', textAlign: 'center' }} onPress={buscarHandle}><Text style={{ color: "#FFFFFF" }}>Buscar valor</Text></Pressable>
-          <Pressable style={{ backgroundColor: '#FF6347', width: 125, borderRadius: 20, marginTop: 20, height: 30, justifyContent: 'center', alignItems: 'center', textAlign: 'center' }} onPress={limparResultado}><Text style={{ color: "#FFFFFF" }}>Limpar valor</Text></Pressable>
+          <Pressable style={{ backgroundColor: '#f23132', width: 125, borderRadius: 20, marginTop: 20, height: 30, justifyContent: 'center', alignItems: 'center', textAlign: 'center' }} onPress={buscarHandle}><Text style={{ color: "#FFFFFF" }}>Buscar valor</Text></Pressable>
+          <Pressable style={{ backgroundColor: '#982131', width: 125, borderRadius: 20, marginTop: 20, height: 30, justifyContent: 'center', alignItems: 'center', textAlign: 'center' }} onPress={limparResultado}><Text style={{ color: "#FFFFFF" }}>Limpar valor</Text></Pressable>
         </View>
-        <Text style={{ textAlign: 'center', color: "#FFFFFF" }}>{`Resultado: ${valorConvertido}`}</Text>
+        <Text style={{ textAlign: 'center', color: "#fff" } }>{`Resultado: ${valorConvertido}`} </Text>
         <StatusBar style="auto" />
       </View>
     </View>
